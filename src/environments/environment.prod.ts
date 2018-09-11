840b3f80029a2b67ecc1146bf39ec939694d1f1f
jasonchan <jiajunc2@illinois.edu>
2018-09-11 13:12:06 -0500

    First commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCcmlsrEPP0qvcrTRn4OSuTfUZxeZYtcUc",
+    authDomain: "jiajun-fitness-tracker.firebaseapp.com",
+    databaseURL: "https://jiajun-fitness-tracker.firebaseio.com",
+    projectId: "jiajun-fitness-tracker",
+    storageBucket: "jiajun-fitness-tracker.appspot.com",
+    messagingSenderId: "78457872151"
+  }
+};
